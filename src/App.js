1080b966176f7a3360424ae6c3ce4168a92ba796
hamilkarr <12uclock@gmail.com>
2024-10-17 05:13:20 +0900

    Pseudo Selector

--- src/App.js
+++ src/App.js
@@ -1,7 +1,47 @@
+import styled, { keyframes } from "styled-components";
+
+const Wrapper = styled.div`
+  display: flex;
+`;
+
+const animation = keyframes`
+  0% {
+    transform: rotate(0deg);
+    border-radius : 0;
+  }
+  50% {
+    transform: rotate(180deg);
+    border-radius : 50%;
+  }
+  100% {
+    transform: rotate(360deg);
+    border-radius : 0;
+  }
+`;
+
+const Box = styled.div`
+  height: 200px;
+  width: 200px;
+  background-color: tomato;
+  animation: ${animation} 2s linear infinite;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  span {
+    font-size: 24px;
+    &:hover {
+      font-size: 36px;
+    }
+  }
+`;
 
 function App() {
   return (
-
+    <Wrapper>
+      <Box>
+        <span>😒</span>
+      </Box>
+    </Wrapper>
   );
 }
 
